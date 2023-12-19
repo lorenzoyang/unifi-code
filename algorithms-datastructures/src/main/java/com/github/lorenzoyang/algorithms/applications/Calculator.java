2748b5b13fb8ce1aa44f62ed022e31607628aeb0
Yang <yangrongwei2002@gmail.com>
2023-12-19 18:46:15 +0100

    updated README

---
+++ algorithms-datastructures/src/main/java/com/github/lorenzoyang/algorithms/applications/Calculator.java
@@ -0,0 +1,12 @@
+package com.github.lorenzoyang.algorithms.applications;
+
+import com.github.lorenzoyang.algorithms.datastructures.LinkedStack;
+
+/**
+ * L'uso della pila per la valutazione di espressioni aritmetiche
+ * nelle espressioni aritmetiche vengono esplicitate tutte le parentesi necessarie
+ * e dunque non si sottointende nessun critetio di precedenza tra le operazioni
+ */
+public class Calculator {
+    private LinkedStack<Number> operands = new LinkedStack<>();
+}
