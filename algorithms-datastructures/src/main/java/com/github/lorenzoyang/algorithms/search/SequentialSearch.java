2d5ad5db6e4586ae28ff1bbc1c2fe0d5ee348f3d
Yang <yangrongwei2002@gmail.com>
2023-12-29 11:22:26 +0100

    algorithms: SequentialSearch added new search

--- algorithms-datastructures/src/main/java/com/github/lorenzoyang/algorithms/search/SequentialSearch.java
+++ algorithms-datastructures/src/main/java/com/github/lorenzoyang/algorithms/search/SequentialSearch.java
@@ -34,4 +34,35 @@ public class SequentialSearch {
         array.remove(array.size() - 1);
         return i >= array.size() ? -1 : i - 1;
     }
+
+    /**
+     * Ricerca sequenziale con sentinella senza l'utilizzo di DynamicArray
+     * restituisce l'indice della prima occorrenza di value
+     */
+    public static <T> int searchSentinel(T[] array, T value) {
+        if (array.length == 0) {
+            return -1;
+        }
+
+        T last = array[array.length - 1];
+        array[array.length - 1] = value;
+
+        int i = 0;
+        while (true) {
+            if (array[i] == value) {
+                break;
+            }
+            i++; // sicuramente arriverà a i = array.length - 1
+        }
+        array[array.length - 1] = last; // ripristino l'ultimo elemento
+
+        // se l'i è arrivato alla fine, e l'ultimo elemento è uguale a value
+        if (i == array.length - 1 && value == last) {
+            return i;
+        } else if (i == array.length - 1 && value != last) {
+            return -1;
+        } else {
+            return i;
+        }
+    }
 }
--- algorithms-datastructures/src/test/java/com/github/lorenzoyang/algorithms/search/SequentialSearchTest.java
+++ algorithms-datastructures/src/test/java/com/github/lorenzoyang/algorithms/search/SequentialSearchTest.java
@@ -77,4 +77,39 @@ class SequentialSearchTest {
         Integer value = 3;
         assertEquals(2, SequentialSearch.search(array, value));
     }
+
+    @Test
+    void searchSentinel_returnsNegative_whenArrayIsEmpty() {
+        Integer[] array = new Integer[0];
+        Integer value = 5;
+        assertEquals(-1, SequentialSearch.searchSentinel(array, value));
+    }
+
+    @Test
+    void searchSentinel_returnsNegative_whenValueIsNotInArray() {
+        Integer[] array = {1, 2, 3, 4, 5};
+        Integer value = 6;
+        assertEquals(-1, SequentialSearch.searchSentinel(array, value));
+    }
+
+    @Test
+    void searchSentinel_returnsIndex_whenValueIsInArray() {
+        Integer[] array = {1, 2, 3, 4, 5};
+        Integer value = 3;
+        assertEquals(2, SequentialSearch.searchSentinel(array, value));
+    }
+
+    @Test
+    void searchSentinel_returnsFirstOccurrenceIndex_whenValueIsRepeatedInArray() {
+        Integer[] array = {1, 2, 3, 4, 5, 3};
+        Integer value = 3;
+        assertEquals(2, SequentialSearch.searchSentinel(array, value));
+    }
+
+    @Test
+    void searchSentinel_returnsLastIndex_whenValueIsLastElement() {
+        Integer[] array = {1, 2, 3, 4, 5};
+        Integer value = 5;
+        assertEquals(4, SequentialSearch.searchSentinel(array, value));
+    }
 }
