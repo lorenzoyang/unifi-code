7e333ffe05af15c2a4fcefead4128ec2d2612cc9
Yang <yangrongwei2002@gmail.com>
2024-01-26 22:04:34 +0100

    javamm: completed 'simulazione2023-02-17' es1

--- javamm-exercises/src/main/java/com/github/lorenzoyang/simulazioni/prova2023_02_17/SommaCifreRipetute.java
+++ javamm-exercises/src/main/java/com/github/lorenzoyang/simulazioni/prova2023_02_17/SommaCifreRipetute.java
@@ -5,8 +5,18 @@ class SommaCifreRipetute {
 
     int sommaCifreRipetute(int numero) {
         int risultato = 0;
-
+        while (numero > 0) {
+            int cifra = numero % 10;
+        }
         return risultato;
+    }
+
+    int[] somma_conta(int numero, int cifra) {
+        int somma, contatore = 0;
+        while (numero % 10 == cifra) {
+            contatore++;
+
+        }
     }
 
     // #fine: javammexercises
