fed8af1c6d088f52f0bb29d796d2658f222670bf
Yang <yangrongwei2002@gmail.com>
2024-01-27 21:18:37 +0100

    javamm: completed 'simulazione2023-02-17'

--- javamm-exercises/src/main/java/com/github/lorenzoyang/simulazioni/prova2023_02_17/RuotaRomboMatrice.java
+++ javamm-exercises/src/main/java/com/github/lorenzoyang/simulazioni/prova2023_02_17/RuotaRomboMatrice.java
@@ -5,10 +5,18 @@ class RuotaRomboMatrice {
 
     int[][] ruotaRomboMatrice(int[][] t, int k) {
         int c = t.length / 2, temp_destra = t[c][c + k];
-        for (int i = 0; i < k; i++) t[c + i][c + k - i] = t[c + i + 1][c + k - i - 1]; // destra
-        for (int i = 0; i < k; i++) t[c + k - i][c - i] = t[c + k - i - 1][c - i - 1]; // sotto
-        for (int i = 0; i < k; i++) t[c - i][c - k + i] = t[c - i - 1][c - k + i + 1]; // sinistra
-        for (int i = 0; i < k; i++) t[c - k + i][c + i] = t[c - k + i + 1][c + i + 1]; // sopra
+        for (int i = 0; i < k; i++) {
+            t[c + i][c + k - i] = t[c + i + 1][c + k - i - 1]; // destra
+        }
+        for (int i = 0; i < k; i++) {
+            t[c + k - i][c - i] = t[c + k - i - 1][c - i - 1]; // sotto
+        }
+        for (int i = 0; i < k; i++) {
+            t[c - i][c - k + i] = t[c - i - 1][c - k + i + 1]; // sinistra
+        }
+        for (int i = 0; i < k; i++) {
+            t[c - k + i][c + i] = t[c - k + i + 1][c + i + 1]; // sopra
+        }
         t[c - 1][c + k - 1] = temp_destra;
         return t;
     }
--- javamm-exercises/src/main/java/com/github/lorenzoyang/simulazioni/prova2023_02_17/SommaCifreRipetute.java
+++ javamm-exercises/src/main/java/com/github/lorenzoyang/simulazioni/prova2023_02_17/SommaCifreRipetute.java
@@ -4,19 +4,25 @@ class SommaCifreRipetute {
     // #inizio: javammexercises
 
     int sommaCifreRipetute(int numero) {
-        int risultato = 0;
+        int risultato = 0, esponente = 0;
         while (numero > 0) {
-            int cifra = numero % 10;
+            int[] valori = sommaConta(numero, numero % 10);
+            risultato += valori[0] * (int) Math.pow(10, esponente);
+            esponente += (valori[0] / 10 > 0) ? 2 : 1;
+            numero /= (int) Math.pow(10, valori[1]);
         }
         return risultato;
     }
 
-    int[] somma_conta(int numero, int cifra) {
-        int somma, contatore = 0;
-        while (numero % 10 == cifra) {
+    // il primo elemento = somma, il secondo elemento = contatore
+    int[] sommaConta(int numero, int cifra) {
+        int somma = 0, contatore = 0;
+        while (numero > 0 && numero % 10 == cifra) {
+            somma += cifra;
             contatore++;
-
+            numero /= 10;
         }
+        return new int[]{somma, contatore};
     }
 
     // #fine: javammexercises
