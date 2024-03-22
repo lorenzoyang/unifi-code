440fd12efe2627f9b80ac1290dac545621bd41eb
Yang <yangrongwei2002@gmail.com>
2024-03-22 22:00:38 +0100

    riscv: removed InsertNullToEndString from ToUpperCase

--- riscv_code/toUppercase.s
+++ riscv_code/toUppercase.s
@@ -1,13 +1,9 @@
 .data
     # la stringa da convertire
-    # \0 funziona da 'null terminator'
-    str: .string "hello world\0"
+    str: .string "hello world hahahah"
 
 .text
 main:
-    la a2, str # argomento della funzione 'insertNullToEndString'
-    jal InsertNullToEndString
-    
     la a2, str # argomento della funzione 'printString'
     jal PrintlnString
     
@@ -69,35 +65,6 @@ PrintlnString:
     lw ra, 0(sp)
     addi sp, sp, 4
     ret
-    
-# argument: a2, la stringa in cui inserire null (0)
-#     stringa passata per 'riferimento'
-# return: none
-InsertNullToEndString:
-    addi sp, sp, -4
-    sw ra, 0(sp)
-    
-    li t0, 92 # t0 = backslash (92 ascii)
-    cycle_1:
-        lbu t1, 0(a2) # carattere attuale
-        bne t1, t0 next_1 # se diverso dal backslash, prossimo ciclo
-        # altrimenti
-        li t3, 48 # t3 = '0' (48 ascii)
-        mv t2, a2 # temp per non perdere a2
-        addi t2, t2, 1 # punta al prossimo carattere
-        lbu t2, 0(t2) # carico il prossimo carattere
-        bne t2, t3 next_1 # se diverso da '0', prossimo ciclo
-        # altrimenti
-        sb zero 0(a2) # inserisco lo zero nel a2
-        
-        end_1:
-            lw ra, 0(sp)
-            addi sp, sp, 4
-            ret
-        
-        next_1:
-            addi a2, a2, 1 # incremento a2 di 1
-            j cycle_1 # prossimo ciclo
         
             
             
